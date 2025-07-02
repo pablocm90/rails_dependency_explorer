466b53e8dfc9df08c3a86fe53ef62750b317ac7d
Pablo <pablocm90@gmail.com>
2025-07-02 16:02:45 +0200

    Quick refactor

--- lib/rails_dependency_explorer/dependency_parser.rb
+++ lib/rails_dependency_explorer/dependency_parser.rb
@@ -9,27 +9,46 @@ module RailsDependencyExplorer
     end
 
     def parse
+      ast = build_ast
+
+      return {} unless ast&.type == :class
+
+      class_name = extract_class_name(ast)
+      dependencies = extract_dependencies(ast)
+      { class_name => dependencies }
+    end
+
+    private
+
+    def build_ast
       parser = Parser::CurrentRuby
-      ast = parser.parse(@ruby_code)
-      if ast.type == :class
-        first_chilfd = ast.children.first
-        label = first_chilfd.children[1].to_s
-        dependencies = ast.children[1..-1].map do |child|
-          find_constants(child)
-        end.flatten
-        { label => dependencies }
+      parser.parse(@ruby_code)
+    end
+
+    def extract_class_name(ast)
+      first_child = ast.children.first
+      first_child.children[1].to_s
+    end
+
+    def extract_dependencies(ast)
+      ast.children[1..-1].map do |child|
+        find_constants(child)
+      end.flatten
+    end
+
+    def find_constants(ast_node)
+      return [] unless ast_node
+      return [] if primitive_type?(ast_node)
+
+      if ast_node.type == :const
+        ast_node.children[1].to_s
+      else
+        ast_node.children.map { |child_node| find_constants(child_node) }.flatten
       end
     end
 
-    def find_constants(node)
-      return [] unless node
-      return [] if node.is_a?(Symbol) || node.is_a?(String) || node.is_a?(Integer)
-
-      if node.type == :const
-        node.children[1].to_s
-      else
-        node.children.map { |child| find_constants(child) }.flatten
-      end
+    def primitive_type?(ast_node)
+      ast_node.is_a?(Symbol) || ast_node.is_a?(String) || ast_node.is_a?(Integer)
     end
   end
 end
