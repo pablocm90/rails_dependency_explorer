ecf9d13449f6aac1e25f164e8cefbb8bac8d5952
Pablo <pablocm90@gmail.com>
2025-07-08 21:00:28 +0200

    Extract dependency graph adapter from dependency visualizer

--- lib/rails_dependency_explorer/output/dependency_visualizer.rb
+++ lib/rails_dependency_explorer/output/dependency_visualizer.rb
@@ -2,18 +2,13 @@
 
 require "set"
 require "json"
+require_relative "dependency_graph_adapter"
 
 module RailsDependencyExplorer
   module Output
     class DependencyVisualizer
       def to_graph(dependency_data)
-        nodes = extract_nodes(dependency_data)
-        edges = extract_edges(dependency_data)
-
-        {
-          nodes: nodes.to_a,
-          edges: edges.to_a
-        }
+        graph_adapter.to_graph(dependency_data)
       end
 
       def to_dot(dependency_data)
@@ -31,37 +26,8 @@ module RailsDependencyExplorer
 
       private
 
-      def extract_nodes(dependency_data)
-        nodes = Set.new
-
-        dependency_data.each do |class_name, dependencies|
-          nodes.add(class_name)
-          add_dependent_nodes(nodes, dependencies)
-        end
-
-        nodes
-      end
-
-      def add_dependent_nodes(nodes, dependencies)
-        dependencies.each do |dependency_hash|
-          dependency_hash.each_key { |dependent_class| nodes.add(dependent_class) }
-        end
-      end
-
-      def extract_edges(dependency_data)
-        edges = Set.new
-
-        dependency_data.each do |class_name, dependencies|
-          add_edges_for_class(edges, class_name, dependencies)
-        end
-
-        edges
-      end
-
-      def add_edges_for_class(edges, class_name, dependencies)
-        dependencies.each do |dependency_hash|
-          dependency_hash.each_key { |dependent_class| edges.add([class_name, dependent_class]) }
-        end
+      def graph_adapter
+        @graph_adapter ||= DependencyGraphAdapter.new
       end
 
       def format_as_dot(edges)
---
+++ test/output/dependency_graph_adapter_test.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require_relative "../test_helper"
+
+class DependencyGraphAdapterTest < Minitest::Test
+  def setup
+    @adapter = RailsDependencyExplorer::Output::DependencyGraphAdapter.new
+  end
+
+  def test_dependency_graph_adapter_converts_single_dependency_to_basic_graph_structure
+    dependency_data = {"Player" => [{"Enemy" => ["health"]}]}
+
+    result = @adapter.to_graph(dependency_data)
+    expected = {
+      nodes: ["Player", "Enemy"],
+      edges: [["Player", "Enemy"]]
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_dependency_graph_adapter_handles_multiple_dependencies
+    dependency_data = {
+      "Player" => [{"Enemy" => ["health"]}, {"Logger" => ["info"]}],
+      "Enemy" => [{"Logger" => ["debug"]}]
+    }
+
+    result = @adapter.to_graph(dependency_data)
+    
+    expected_nodes = ["Player", "Enemy", "Logger"]
+    expected_edges = [["Player", "Enemy"], ["Player", "Logger"], ["Enemy", "Logger"]]
+
+    assert_equal expected_nodes.sort, result[:nodes].sort
+    assert_equal expected_edges.sort, result[:edges].sort
+  end
+
+  def test_dependency_graph_adapter_handles_empty_dependency_data
+    dependency_data = {}
+
+    result = @adapter.to_graph(dependency_data)
+    expected = {
+      nodes: [],
+      edges: []
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_dependency_graph_adapter_handles_class_with_no_dependencies
+    dependency_data = {"Player" => []}
+
+    result = @adapter.to_graph(dependency_data)
+    expected = {
+      nodes: ["Player"],
+      edges: []
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_dependency_graph_adapter_deduplicates_nodes_and_edges
+    dependency_data = {
+      "Player" => [{"Enemy" => ["health"]}, {"Enemy" => ["damage"]}]
+    }
+
+    result = @adapter.to_graph(dependency_data)
+    expected = {
+      nodes: ["Player", "Enemy"],
+      edges: [["Player", "Enemy"]]
+    }
+
+    assert_equal expected, result
+  end
+end
