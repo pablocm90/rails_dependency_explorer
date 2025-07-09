0cbcbb7454cae7f89d3b70d0f423aebef221c86a
Pablo <pablocm90@gmail.com>
2025-07-09 14:38:05 +0200

    Break up some complexity

--- lib/rails_dependency_explorer/analysis/circular_dependency_analyzer.rb
+++ lib/rails_dependency_explorer/analysis/circular_dependency_analyzer.rb
@@ -34,23 +34,38 @@ module RailsDependencyExplorer
       end
 
       def find_cycles_dfs(node, graph, visited, rec_stack, path, cycles)
+        mark_node_as_visited(node, visited, rec_stack, path)
+
+        graph[node].each do |neighbor|
+          process_neighbor(neighbor, graph, visited, rec_stack, path, cycles)
+        end
+
+        unmark_node(node, rec_stack, path)
+      end
+
+      def mark_node_as_visited(node, visited, rec_stack, path)
         visited.add(node)
         rec_stack.add(node)
         path.push(node)
+      end
 
-        graph[node].each do |neighbor|
-          if !visited.include?(neighbor)
-            find_cycles_dfs(neighbor, graph, visited, rec_stack, path, cycles)
-          elsif rec_stack.include?(neighbor)
-            # Found a cycle
-            cycle_start_index = path.index(neighbor)
-            if cycle_start_index
-              cycle = path[cycle_start_index..] + [neighbor]
-              cycles << cycle unless cycles.include?(cycle)
-            end
-          end
+      def process_neighbor(neighbor, graph, visited, rec_stack, path, cycles)
+        if !visited.include?(neighbor)
+          find_cycles_dfs(neighbor, graph, visited, rec_stack, path, cycles)
+        elsif rec_stack.include?(neighbor)
+          extract_cycle(neighbor, path, cycles)
         end
+      end
 
+      def extract_cycle(neighbor, path, cycles)
+        cycle_start_index = path.index(neighbor)
+        if cycle_start_index
+          cycle = path[cycle_start_index..] + [neighbor]
+          cycles << cycle unless cycles.include?(cycle)
+        end
+      end
+
+      def unmark_node(node, rec_stack, path)
         rec_stack.delete(node)
         path.pop
       end
--- lib/rails_dependency_explorer/analysis/dependency_depth_analyzer.rb
+++ lib/rails_dependency_explorer/analysis/dependency_depth_analyzer.rb
@@ -30,39 +30,61 @@ module RailsDependencyExplorer
 
       def extract_all_nodes(graph)
         all_nodes = Set.new(@dependency_data.keys)
-        graph.each { |node, neighbors|
-          all_nodes.add(node)
-          neighbors.each { |n| all_nodes.add(n) }
-        }
+        add_graph_nodes_to_set(graph, all_nodes)
         all_nodes
+      end
+
+      def add_graph_nodes_to_set(graph, all_nodes)
+        graph.each do |node, neighbors|
+          all_nodes.add(node)
+          add_neighbors_to_set(neighbors, all_nodes)
+        end
+      end
+
+      def add_neighbors_to_set(neighbors, all_nodes)
+        neighbors.each { |neighbor| all_nodes.add(neighbor) }
       end
 
       def build_reverse_adjacency_list(graph)
         reverse_graph = Hash.new { |h, k| h[k] = [] }
-
-        graph.each do |node, neighbors|
-          neighbors.each do |neighbor|
-            reverse_graph[neighbor] << node unless reverse_graph[neighbor].include?(node)
-          end
-        end
-
+        populate_reverse_graph(graph, reverse_graph)
         reverse_graph
+      end
+
+      def populate_reverse_graph(graph, reverse_graph)
+        graph.each do |node, neighbors|
+          add_reverse_dependencies(node, neighbors, reverse_graph)
+        end
+      end
+
+      def add_reverse_dependencies(node, neighbors, reverse_graph)
+        neighbors.each do |neighbor|
+          add_unique_dependency(reverse_graph, neighbor, node)
+        end
+      end
+
+      def add_unique_dependency(reverse_graph, neighbor, node)
+        reverse_graph[neighbor] << node unless reverse_graph[neighbor].include?(node)
       end
 
       def calculate_node_depth(node, reverse_graph, memo)
         return memo[node] if memo.key?(node)
 
-        # If no one depends on this node, depth is 0 (root level)
         dependents = reverse_graph[node] || []
-        if dependents.empty?
-          memo[node] = 0
-          return 0
-        end
+        depth = calculate_depth_from_dependents(dependents, reverse_graph, memo)
+        memo[node] = depth
+        depth
+      end
 
-        # Depth is 1 + max depth of dependents
-        max_dependent_depth = dependents.map { |dep| calculate_node_depth(dep, reverse_graph, memo) }.max || 0
-        memo[node] = max_dependent_depth + 1
-        memo[node]
+      def calculate_depth_from_dependents(dependents, reverse_graph, memo)
+        return 0 if dependents.empty?
+
+        max_dependent_depth = find_max_dependent_depth(dependents, reverse_graph, memo)
+        max_dependent_depth + 1
+      end
+
+      def find_max_dependent_depth(dependents, reverse_graph, memo)
+        dependents.map { |dep| calculate_node_depth(dep, reverse_graph, memo) }.max || 0
       end
     end
   end
